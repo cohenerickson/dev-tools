3991a85dc9654d5cb12788109687acef058198d4
Cohen Erickson <72945444+cohenerickson@users.noreply.github.com>
2022-02-19 22:27:47 +0000

    e

--- devtools/console/helpers/errors.js
+++ devtools/console/helpers/errors.js
@@ -1,8 +1,6 @@
-
-
 export default function (e) {
   let window = document.getElementById("devtools").contentWindow.document;
   let count = parseInt(window.getElementById("label-x").innerText);
   window.getElementById("label-x").innerHTML = "<img src='./assets/icons/x.svg'>" + (count+1);
-  console.error(e.error.stack);
+  console.error(e);
 }
